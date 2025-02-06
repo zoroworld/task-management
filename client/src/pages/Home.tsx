d3d214ed932a8a0108a694000ebf9619c0267652
zoroworld <pandamanishwork@gmail.com>
2025-02-06 19:44:14 +0530

    task management

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>Welcome to the Task Manager</h1>
+      <h1>Welcome to the Task Management</h1>
       <p>Get started by logging in or signing up.</p>
       <Link to="/login">Login</Link> | <Link to="/signup">Signup</Link>
     </div>
